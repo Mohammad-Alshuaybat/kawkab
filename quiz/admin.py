3a985803339e07538131572d9ffe766c9fc37935
Malek Aldebsi <86883738+Malek-Aldebsi@users.noreply.github.com>
2023-03-02 18:15:07 +0300

    ...

--- quiz/admin.py
+++ quiz/admin.py
@@ -3,7 +3,7 @@ from import_export.admin import ExportActionMixin
 from .models import Subject, QuestionLevel, Module, Lesson, \
     AdminAnswer, UserAnswer, AdminFinalAnswer, UserFinalAnswer, AdminMultipleChoiceAnswer, \
     UserMultipleChoiceAnswer, FinalAnswerQuestion, MultipleChoiceQuestion, Solution, AdminQuiz, UserQuiz, Question, \
-    HeadLine, H1
+    HeadLine, H1, LastImageName, Author, HeadLineInst
 
 
 class ExportAllFields(ExportActionMixin, admin.ModelAdmin):
@@ -11,25 +11,29 @@ class ExportAllFields(ExportActionMixin, admin.ModelAdmin):
 
 
 admin.site.register(Subject, ExportAllFields)
-# admin.site.register(Tag, ExportAllFields)  abstract
+admin.site.register(Module, ExportAllFields)
+admin.site.register(Lesson, ExportAllFields)
 
+admin.site.register(Author, ExportAllFields)
+
+admin.site.register(QuestionLevel, ExportAllFields)
 admin.site.register(H1, ExportAllFields)
 admin.site.register(HeadLine, ExportAllFields)
 
-# admin.site.register(SkillInst, ExportAllFields)
-admin.site.register(Module, ExportAllFields)
-admin.site.register(Lesson, ExportAllFields)
-# admin.site.register(Answer, ExportAllFields)  abstract
-admin.site.register(AdminAnswer, ExportAllFields)
-admin.site.register(UserAnswer, ExportAllFields)
+admin.site.register(HeadLineInst, ExportAllFields)
 admin.site.register(AdminFinalAnswer, ExportAllFields)
+
+
+# admin.site.register(SkillInst, ExportAllFields)
+# admin.site.register(Answer, ExportAllFields)  abstract
 admin.site.register(UserFinalAnswer, ExportAllFields)
 admin.site.register(AdminMultipleChoiceAnswer, ExportAllFields)
 admin.site.register(UserMultipleChoiceAnswer, ExportAllFields)
-admin.site.register(Question, ExportAllFields)  # abstract
+
 admin.site.register(FinalAnswerQuestion, ExportAllFields)
 admin.site.register(MultipleChoiceQuestion, ExportAllFields)
+
 admin.site.register(Solution, ExportAllFields)
-# admin.site.register(Quiz, ExportAllFields)  abstract
-admin.site.register(AdminQuiz, ExportAllFields)
+
+admin.site.register(AdminQuiz, ExportAllFields)  # abstract
 admin.site.register(UserQuiz, ExportAllFields)
