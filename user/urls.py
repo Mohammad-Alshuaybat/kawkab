39ff3b1c26e850a9d9d31244885d9ef6d887b75d
malek aldebsi <malek315@gmail.com>
2023-05-23 21:01:21 +0300

    ...

--- user/urls.py
+++ user/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import sign_up, log_in, dashboard, edit_tasks
+from .views import sign_up, log_in, dashboard, edit_tasks, check_new_account_info
 
 urlpatterns = [
+    path('check_new_account_info/', check_new_account_info),
     path('sign_up/', sign_up),
     path('log_in/', log_in),
     path('dashboard/', dashboard),
